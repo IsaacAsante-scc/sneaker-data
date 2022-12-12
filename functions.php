e354edd032aa0fa47611bac5bd9bffc3adbd10cf
Isaac Asante <W0725009@myscc.ca>
2022-12-11 20:50:36 -0500

    Add Footer
    Added wp_footer function to enqueue jquery, popper, bootstrap and main.js dynamically.

--- functions.php
+++ functions.php
@@ -15,7 +15,13 @@ function sneakerdata_register_styles(){
 
 
 function sneakerdata_register_scripts(){
-       
+
+    wp_enqueue_script('sneakerdata-jquery', "https://code.jquery.com/jquery-3.4.1.slim.min.js", array(), '3.4.1', true);
+    wp_enqueue_script('sneakerdata-popper', "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js", array(), '1.16.0', true);
+    wp_enqueue_script('sneakerdata-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js", array(), '4.4.1', true);
+    wp_enqueue_script('sneakerdata-main', get_template_directory_uri() . "/assets/js/main.js", array(), '1.0', true);
+
+
 }
     
     add_action( 'wp_enqueue_scripts', 'sneakerdata_register_scripts');
